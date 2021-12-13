32f6e26bb507bf193c0b60cf776f4f8cb882a69b
Raphael <89421168+blocen@users.noreply.github.com>
2021-12-13 23:46:47 +0100

    search names

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /** Connect to Moralis server */
-const serverUrl = "";
-const appId = "";
+
 Moralis.start({ serverUrl, appId });
 
 /** Add from here down */
@@ -9,8 +8,8 @@ async function login() {
   if (!user) {
    try {
       user = await Moralis.authenticate({ signingMessage: "hello NiFTy" })
-      console.log(user)
-      console.log(user.get('ethAddress'))
+      console.log("user: ", user)
+      console.log("user address: ", user.get('ethAddress'))
    } catch(error) {
      console.log(error)
    }
@@ -24,5 +23,13 @@ async function logout() {
 
 document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logout;
+document.getElementById("btn-search").onclick = searchNFTs;
 
+async function searchNFTs() {
+  const options = { q: "asdf", chain: "bsc", filter: "name" };
+  console.log(options);
+  const NFTs = await Moralis.Web3API.token.searchNFTs(options);
+  console.log(NFTs);
+}
 
+searchNFTs();
