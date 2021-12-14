9a5f3c2cbdcf8a19017c4a93c1b932ba34ae2a97
Raphael <89421168+blocen@users.noreply.github.com>
2021-12-14 20:11:07 +0100

    NFT list

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-/** Connect to Moralis server */
+/** Connect to Moralis server */Moralis.start({ serverUrl, appId });
 
-Moralis.start({ serverUrl, appId });
-
-/** Add from here down */
 async function login() {
   let user = Moralis.User.current();
   if (!user) {
@@ -23,13 +20,85 @@ async function logout() {
 
 document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logout;
-document.getElementById("btn-search").onclick = searchNFTs;
 
-async function searchNFTs() {
-  const options = { q: "asdf", chain: "bsc", filter: "name" };
-  console.log(options);
-  const NFTs = await Moralis.Web3API.token.searchNFTs(options);
-  console.log(NFTs);
+document.getElementById("btn-search-name").onclick = searchNFTsByName;
+
+async function searchNFTsByName() {
+  // todo: check min 3 characters
+  let search_param_name = document.getElementById("search-param-name").value;
+  // console.log(search_param_name);
+  const options = { q: search_param_name, chain: "bsc", filter: "name" };
+  // console.log(options);
+
+  let resultsByName = document.getElementById('resultsByName')
+  resultsByName.innerText = "";
+  let content = "";
+  let results;
+
+  try {
+    results = await Moralis.Web3API.token.searchNFTs(options);
+    // console.log("results1: ", results);
+  } catch (error) {
+    console.log(error);
+    resultsByName.innerHTML = `<p>No results...</p>`;
+    return;
+  }
+  console.log(results.result);
+  if (results.result.length == 0) {
+    resultsByName.innerHTML = `<p>No results...</p>`;
+    return;
+  }
+
+  // console.log(results.result.length);
+  result = results.result;
+  for (let i = 0; i < result.length && i < 15; i++) {
+    // console.log(i);
+    token_address = result[i].token_address;
+    metadata_name = result[i].metadata;
+    created_at    = result[i].created_at;
+    console.log(token_address, metadata_name, created_at);
+    const div = document.createElement("div");
+    // div.innerHTML = token_address + metadata_name + created_at;
+    div.innerHTML = 
+    `<h6>${token_address}</h6>
+     <p>${created_at.toLocaleString()}</p>
+     <p>${metadata_name}</p>
+     <hr/>`
+
+    resultsByName.appendChild(div);
+  }
+  // renderResults();
 }
 
-searchNFTs();
+// // display NFT list...
+// async function renderResults() {
+//   let commentSection = document.getElementById('resultsByName')
+//   commentSection.innerText = "";
+//   let result;
+
+//   // <ul class="list-group">
+//   //   <li class="list-group-item">An item</li>
+//   // </ul>
+
+//   try {
+//       result = await Moralis.Cloud.run("getData", {});
+//   } catch (error) {
+//       commentSection.innerHTML = `
+//                   <p>No comments, you may not have permission to view them</p>
+//                   `
+//       return;
+//   }
+//   if (result) {
+//       for (let i = 0; i < result.length; i++) {
+//           let _user = result[i].attributes.writer;
+//           console.log(result[i])
+//           const content = document.createElement("div")
+//           content.innerHTML = `
+//           <h6>${result[i].get('content')}</h6>
+//           <p>(Posted by ${_user}: ${result[i].get('createdAt').toLocaleString()})</p>
+//                           <hr/>
+//                           `
+//           commentSection.appendChild(content)
+//       }
+//   }
+// }
